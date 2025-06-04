9c0540a0e3d9c60a98aa16f9560c664ba24551dc
Jim Clark <jim.clark@generalassemb.ly>
2025-06-04 08:16:11 -0700

    Set up project

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const path = require('path'); // Built into Node
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+
+// Process the secrets/config vars in .env
+require('dotenv').config();
+
+// Connect to the database
+require('./db');
+
+app.use(logger('dev'));
+// Serve static assets from the frontend's built code folder (dist)
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// Note that express.urlencoded middleware is not needed
+// because forms are not submitted!
+app.use(express.json());
+
+// API Routes
+
+
+// Use a "catch-all" route to deliver the frontend's production index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`The express app is listening on ${port}`);
+});
